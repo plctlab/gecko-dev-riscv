7ab10046ad754c7446620dd8fb812f352a55cc46
gaojie <gaojie@nfschina.com>
2022-08-25 10:54:27 +0800

    zlib

--- build-riscv.sh
+++ build-riscv.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-export LDFLAGS=-L/lvm/1/code/github/zlib-1.2.11.dfsg
-export CXXFLAGS=-I/lvm/1/code/github/zlib-1.2.11.dfsg
-export CFLAGS=-I/lvm/1/code/github/zlib-1.2.11.dfsg
+export LDFLAGS=-L${zlib-path}
+export CXXFLAGS=-I${zlib-path}
+export CFLAGS=-I${zlib-path}
 
 export CC=/usr/bin/riscv64-linux-gnu-gcc
 export CXX=/usr/bin/riscv64-linux-gnu-g++
